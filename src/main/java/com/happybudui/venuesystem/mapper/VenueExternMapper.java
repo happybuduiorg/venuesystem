6cb85e7b21abba817073708b2ad30bced0bd6112
Snowren1123 <39525249+snowren1123@users.noreply.github.com>
2018-12-03 19:37:55 +0800

    commit rjh
    venueExternMapper

---
+++ src/main/java/com/happybudui/venuesystem/mapper/VenueExternMapper.java
@@ -0,0 +1,33 @@
+package com.happybudui.venuesystem.mapper;
+
+import com.happybudui.venuesystem.entity.VenueExternEntity;
+import org.apache.ibatis.annotations.*;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+
+import java.util.List;
+
+@Mapper
+@CacheConfig(cacheNames = "venueextern")
+public interface VenueExternMapper {
+
+    @Insert("insert into venueextern(venueid, dayofweek, venueslot, arearemain) values(#{venueId}, #{dayOfWeek}, #{venueSlot}, #{areaRemain})")
+    int insertVenueExternInfo(VenueExternEntity venueExternEntity);
+
+    @Select("select * from venueextern")
+    List<VenueExternEntity> getAllVenueExternInfo();
+
+    @Cacheable(key="#p0")
+    @Select("select * from venueextern where venueid=#{venueId}")
+    List<VenueExternEntity> getVenueExternInfoById(@Param("venueId")int venueId);
+
+    @Cacheable()
+    @Select("select arearemain from venueextern where venueid = #{venueId} and dayofweek = #{dayOfWeek} and venueslot = #{venueSlot}")
+    int getVenueAreaRemain(@Param("venueId")int venueId, @Param("dayOfWeek")int dayOfWeek, @Param("venueSlot")int venueSlot);
+
+    @CachePut(key="#p0")
+    @Update("update venueextern set arearemain = #{areaRemain} where venueid = #{venueId} and dayofweek = #{dayOfWeek} and venueslot = #{venueSlot}")
+    int updateVenueAreaRemain(@Param("venueId")int venueId,@Param("areaRemain")int areaRemain, @Param("dayOfWeek")int dayOfWeek, @Param("venueSlot")int venueSlot);
+
+}
